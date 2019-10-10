bda78244869d7047f094046d1364cc7df4903b4b
Misayou <2078407064@qq.com>
2019-10-10 21:49:32 +0800

    Misayou

--- 201731062415/WordCount/FileOutputProcess.cs
+++ 201731062415/WordCount/FileOutputProcess.cs
@@ -16,7 +16,8 @@ namespace WordCount
         public override bool process(Context context)
         {
             Console.WriteLine("内容输出到文件");
-            string path = @"E:\我的vs程序\done by myself\WordCount\WordCount\test.txt";
+            //输出文件位置
+            string path = @"E:\Gitprogram\WordCount\201731062415\WordCount\test.txt";
             FileInfo fileInfo = new FileInfo(path);
             StreamWriter sw = fileInfo.AppendText();
             foreach (KeyValuePair<string, int> entry in context.WordCount)
